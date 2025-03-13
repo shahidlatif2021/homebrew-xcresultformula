1d59a81c18598d48614c4f8406778db1376b717e
Shahid Latif <shahid.latif@almosafer.com>
2025-03-13 11:31:21 +0400

    update formula file

--- xcresult-formula.rb
+++ xcresult-formula.rb
@@ -1,17 +1,17 @@
 class xcresulttool < Formula
   desc "This tool will convert xcode report into JSON. Later on PDF support will be added"
   homepage "https://github.com/shahidlatif2021/xcresultformula"
-  url "https://github.com/yourusername/your-cli-tool/releases/download/v1.0.0/your-cli-tool.tar.gz"
-  sha256 "your_sha256_checksum_here"
+  url "https://github.com/shahidlatif2021/xcresultformula/blob/develop/XCResultParser.tar.gz"
+  sha256 "e6223d81fc5661502f942df5d8c24c9cdb21e2f2864ba6fad453bb55dd85f269"
   license "MIT"
 
   depends_on "swift" # If your app depends on Swift
 
   def install
-    bin.install "your-cli-tool"
+    bin.install "XCResultParser"
   end
 
   test do
-    system "#{bin}/your-cli-tool", "--version"
+    system "#{bin}/XCResultParser", "--version"
   end
 end
