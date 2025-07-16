c28221ee35b123d2ddad4d5abdf806cc3e318736
Shahid Latif <shahid.latif@almosafer.com>
2025-07-16 22:36:26 +0400

    update issue where reports are not coming

--- Formula/xcresulttool.rb
+++ Formula/xcresulttool.rb
@@ -1,9 +1,9 @@
 class Xcresulttool < Formula
   desc "This tool will convert xcode report into JSON. Later on PDF support will be added"
   homepage "https://github.com/shahidlatif2021/xcresultformula"
-  version "2.0.2"
-  url "https://github.com/shahidlatif2021/homebrew-xcresultformula/releases/download/v2.0.2/XCResultParser.tar.gz"
-  sha256 "5ad41d328127b2a69dc4ab93741a3902ea67864b3445fd2293bb204273a5eb66"
+  version "2.0.3"
+  url "https://github.com/shahidlatif2021/homebrew-xcresultformula/releases/download/v2.0.3/XCResultParser.tar.gz"
+  sha256 "cd5483cbb6b45c2f015969b3397caf32e8a933c0d2faa73e4fbb680a275322d4"
   license "MIT"
 
 
