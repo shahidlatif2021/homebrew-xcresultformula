bb6e203ecd80a89de61a3d5f1259a4742dccd79c
Shahid Latif <shahid.latif@almosafer.com>
2025-07-17 10:11:47 +0400

    update again file

--- Formula/xcresulttool.rb
+++ Formula/xcresulttool.rb
@@ -3,7 +3,7 @@ class Xcresulttool < Formula
   homepage "https://github.com/shahidlatif2021/xcresultformula"
   version "2.0.3"
   url "https://github.com/shahidlatif2021/homebrew-xcresultformula/releases/download/v2.0.3/XCResultParser.tar.gz"
-  sha256 "cd5483cbb6b45c2f015969b3397caf32e8a933c0d2faa73e4fbb680a275322d4"
+  sha256 "7fcf727b792be911f36088df1e96ce7645a1d6d3168a14efb5caf7ea3b459eaa"
   license "MIT"
 
 
