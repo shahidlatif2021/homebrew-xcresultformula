ffc2b5f56949c8853466c78918a1a3ac7086041b
Shahid Latif <shahid.latif@almosafer.com>
2026-01-14 11:51:12 +0400

    update version

--- Formula/xcresulttool.rb
+++ Formula/xcresulttool.rb
@@ -1,9 +1,9 @@
 class Xcresulttool < Formula
   desc "This tool will convert xcode report into JSON. Later on PDF support will be added"
   homepage "https://github.com/shahidlatif2021/xcresultformula"
-  version "2.1.0"
-  url "https://github.com/shahidlatif2021/homebrew-xcresultformula/releases/download/v2.1.0/XCResultParser.tar.gz"
-  sha256 "cd8edd69055a7376994123896935c14053911e6d7a4bafa6340c8bea9270302e"
+  version "2.1.1"
+  url "https://github.com/shahidlatif2021/homebrew-xcresultformula/releases/download/v2.1.1/XCResultParser.tar.gz"
+  sha256 "e1918a4a85145c661efcdffa23e1956dbc402d5683ff68c7b6f650da9eb4b0a5"
   license "MIT"
 
 
